6a0119f5b731238ed378b2062784dba904c818b4
neha45556 <neha45556@gmail.com>
2016-08-01 11:34:25 -0700

    Add files via upload

---
+++ myHomepage.js
@@ -0,0 +1,8 @@
+var displayjp = function(){
+	var string = "jackson-pollock"
+	document.getElementById("jp").innerHTML = 'https://www.khanacademy.org/computer-programming/jackson-pollock/4530835157024768">Jackson Pollock'
+}
+
+var changePicture = function(){
+	
+}
