d4a11dc779cb270531e883b4923027dfdbf648ee
neha45556 <neha45556@gmail.com>
2016-08-05 14:03:45 -0700

    Add files via upload

--- myHomepage.js
+++ myHomepage.js
@@ -6,3 +6,13 @@ var displayjp = function(){
 var changePicture = function(){
 	
 }
+
+function bigImg(x) {
+    x.style.height = "220px";
+    x.style.width = "220px";
+}
+
+function normalImg(x) {
+    x.style.height = "150px";
+    x.style.width = "150px";
+}
